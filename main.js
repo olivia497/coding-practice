a3749e1a479b162642c69ef6fa33e671e26ed7ad
Olivia <oliviafedato@gmail.com>
2024-03-31 10:37:09 -0600

    more coding practice

--- main.js
+++ main.js
@@ -149,12 +149,12 @@ function multiply(a, b){
 
 
 
-//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//create function that returns even for even numbers and off for odd numbers
 function evenOrOdd(number) {
   if(number % 2 === 0){
-    return "Even"
+    return 'Even'
   }else{
-    return "Odd"
+    return 'Odd'
   }
 }
 
