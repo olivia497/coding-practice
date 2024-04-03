1ba76ce32c05280d6e9d7403ea2899c39f163acf
Olivia <oliviafedato@gmail.com>
2024-04-03 12:44:04 -0600

    coding practice

--- main.js
+++ main.js
@@ -187,3 +187,14 @@ function getGrade (s1, s2, s3) {
 function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
+
+//Let's play! You have to return which player won! In case of a draw return Draw!.
+const rps = (p1, p2) => {
+  if(p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' || p1 === 'rock' && p2 === 'scissors'){
+    return 'Player 1 won!' 
+  }else if(p1 === 'paper' && p2 === 'scissors' || p1 === 'rock' && p2 === 'paper' || p1 === 'scissors' && p2 === 'rock'){
+    return 'Player 2 won!'
+  }else{
+    return 'Draw!'
+  }
+};
