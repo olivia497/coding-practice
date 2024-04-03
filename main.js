060cc0e6cf3f47e25cbae71574cf31f9e3d732e2
Olivia <oliviafedato@gmail.com>
2024-04-02 18:30:33 -0600

    today's coding practice

--- main.js
+++ main.js
@@ -180,3 +180,10 @@ function getGrade (s1, s2, s3) {
     return 'A'
   }
 }
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
