7a90d9d959abc3a94d74b15767b2f13705cec629
Olivia <oliviafedato@gmail.com>
2024-04-06 10:47:13 -0600

    coding practice for today

--- main.js
+++ main.js
@@ -204,3 +204,8 @@ function removeChar(str){
   newStr = str.slice(1, -1)
   return newStr
 };
+
+//Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+function smash (words) {
+  return words.join(' ')
+};
