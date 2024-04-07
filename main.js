5de44f9157e1a25686eb01897670b68cbb5abdf5
Olivia <oliviafedato@gmail.com>
2024-04-07 17:48:28 -0600

    coding pratice

--- main.js
+++ main.js
@@ -219,3 +219,19 @@ function removeChar(str){
 function smash (words) {
   return words.join(' ')
 };
+
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2){
+  if(operation === '+'){
+    return value1 + value2
+  }else if(operation === '-'){
+    return value1 - value2
+  }else if(operation === '*'){
+    return value1 * value2
+  }else if(operation ==='/'){
+    return value1 / value2
+  }
+}
