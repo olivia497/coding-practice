2c98ba4caced0da5caa7e49e22acf8cbfddcf6c8
Olivia <oliviafedato@gmail.com>
2024-04-08 12:54:55 -0600

    coding practice

--- main.js
+++ main.js
@@ -174,6 +174,8 @@ function numberToString(num) {
   return num.toString()
 }
 
+const numberToString = num => num.toString()
+
 // Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 function getGrade (s1, s2, s3) {
   let sum = s1 + s2 + s3
