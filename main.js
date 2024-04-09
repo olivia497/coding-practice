95ec427f9193c28a205e8875c6d1832176266d85
Olivia <oliviafedato@gmail.com>
2024-04-09 12:05:33 -0600

    coding practice added

--- main.js
+++ main.js
@@ -195,6 +195,22 @@ function getGrade (s1, s2, s3) {
   }
 }
 
+function getGrade (s1, s2, s3) {
+  let score = (s1+s2+s3)/3
+  
+   if(score <= 0 || score < 60){
+    return 'F'
+  }else if(score <= 60 || score < 70){
+    return 'D'
+  }else if(score <= 70 || score < 80){
+    return 'C'
+  }else if(score <= 80 || score < 90){
+    return 'B'
+  }else{
+    return 'A'
+  }
+}
+
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 
