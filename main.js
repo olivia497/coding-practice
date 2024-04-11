cd953de589d32fa3884d1ac7d620e353ca2a67c0
Olivia <oliviafedato@gmail.com>
2024-04-10 18:27:26 -0600

    more coding practice

--- main.js
+++ main.js
@@ -307,3 +307,16 @@ function fakeBin(x){
   }
   return result;
 }
+
+//You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again. Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+function updateLight(current) {
+  
+  if (current === 'green'){
+    return 'yellow'
+  } else if (current === 'yellow') {
+    return'red'
+  } else {
+    return 'green'
+  }
+}
