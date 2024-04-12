ec5ea46d7122075857ebdcd3c0d23b7191cec686
Olivia <oliviafedato@gmail.com>
2024-04-12 17:07:12 -0600

    coding practice

--- main.js
+++ main.js
@@ -268,6 +268,8 @@ function boolToWord( bool ){
   }
 }
 
+const boolToWord = bool => bool === true ? 'yes' : 'no'
+
 // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
 const min = list => Math.min(...list)
