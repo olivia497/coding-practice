4a3523050c743d8c79d85a5648c724d347bdae1b
Olivia <oliviafedato@gmail.com>
2024-04-25 09:58:31 -0600

    tests updated

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-function isPalindrome(x) {
-  let checkX = x.toString(x.split('').reverse().join(''))
-  if(x === checkX){
-    return true
-  }else{
-    return false
-  }
-}
+// function isPalindrome(x) {
+//   let checkX = x.toString(x.split('').reverse().join(''))
+//   if(x === checkX){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
+
+const isPalindrome = x => x.toLowerCase() === x.split('').reverse().join('').toLowerCase() ? true : false
+
+console.log(isPalindrome('hello'))
