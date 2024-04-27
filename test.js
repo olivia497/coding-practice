ea393d11c7e903e2b856a5ae01b98512d7aa9379
Olivia <oliviafedato@gmail.com>
2024-04-27 10:20:55 -0600

    coding practice tested & passed

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
-// function isPalindrome(x) {
-//   let checkX = x.toString(x.split('').reverse().join(''))
-//   if(x === checkX){
-//     return true
-//   }else{
-//     return false
-//   }
-// }
-
-// const isPalindrome = x => x.toLowerCase() === x.split('').reverse().join('').toLowerCase() ? true : false
-
-// console.log(isPalindrome('hello'))
-
-
-const otherAngle = (a,b) => 180 - a - b
+String.prototype.isUpperCase = function() {
+  return this == this.toUpperCase()
+}
