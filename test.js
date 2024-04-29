c7142903882ec96d1839ec9d8b1c374710442941
Olivia <oliviafedato@gmail.com>
2024-04-29 12:54:13 -0600

    cleared test

--- test.js
+++
@@ -1,9 +0,0 @@
-const haystack = ['bison', 'apple', 'needle', 'chair']
-
-function findNeedle(haystack) {
-  let needleIndex = haystack.indexOf('needle');
-  console.log(`found in position ${needleIndex}`)
-};
-
-findNeedle(haystack);
-
