548c78c59e216b1598c218c5d2329cff511dea9a
Olivia <oliviafedato@gmail.com>
2024-05-02 07:48:36 -0600

    coding practice added

--- main.js
+++ main.js
@@ -448,3 +448,7 @@ function abbrevName(name){
   let initials = name.split(" "); //Splitting the string at the space between first and last name
   return(initials[0][0] + "." + initials[1][0]).toUpperCase(); //Returns the first letter of first name and first letter of last name
 }
+
+// Write a function "greet" that returns "hello world!"
+
+const greet = phrase => "hello world!";
